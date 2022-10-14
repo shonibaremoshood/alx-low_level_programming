1d8a15b5ce86788e57ae2bb4ad8464523d8115a3
shonibaremoshood <shonibaremoshood@gmail.com>
2022-10-14 01:27:29 -0700

    compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry
- * Return value - 0
+ * Return - 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
