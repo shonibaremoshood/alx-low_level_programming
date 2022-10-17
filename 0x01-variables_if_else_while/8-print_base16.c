3cf9f35ce2fa57ce62bd85cc00585e3510377a62
shonibaremoshood <shonibaremoshood@gmail.com>
2022-10-17 12:52:52 -0700

    add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - number of base16 in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char base;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (base = 'a'; base <= 'f'; base++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
