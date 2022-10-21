142699ae61734321befc618a133f7b72078e7748
shonibaremoshood <shonibaremoshood@gmail.com>
2022-10-21 07:47:46 -0700

    modify 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,13 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
+			{
 				_putchar('1' + n);
-			_putchar(n % 10 + '0');
+			}
+			else
+			{
+				_putchar(n);
+			}
 		}
 		counter++;
 		_putchar('\n');
